55bb2c586c7935dbfb80f32d8d568fdf6e5bf239
Liu Sun <sunliu@yxnu.edu.cn>
2023-05-15 22:24:49 +0800

    Update ensembl.R

--- ensembl.R
+++ ensembl.R
@@ -1,13 +1,14 @@
 library(httr)
 library(jsonlite)
-variant_recoder <- function(id, ...) {
-  response <- GET(paste0("https://rest.ensembl.org/variant_recoder/human/", id),
-                  query = list(...), content_type_json(), accept_json())
-  return(fromJSON(toJSON(content(response))))
-}
 
-variant_recoder <- function(id, ...) {
-  response <- POST("https://rest.ensembl.org/variant_recoder/human", body = list(ids = id),
-                   encode = "json", query = list(...), content_type_json(), accept_json())
+variant_recoder <- function(id,species="human", content_type="application/json", ...) {
+  response <- GET("https://rest.ensembl.org", path=paste("variant_recoder", species, id, sep = "/"),
+                  query = list(...), content_type(content_type))
   return(fromJSON(toJSON(content(response))))
 }
+vr<-variant_recoder("NP_001361433.1:p.Asp512Asn")
+# variant_recoder <- function(id, ...) {
+#   response <- POST("https://rest.ensembl.org/variant_recoder/human", body = list(ids = id),
+#                    encode = "json", query = list(...), content_type_json(), accept_json())
+#   return(fromJSON(toJSON(content(response))))
+# }
